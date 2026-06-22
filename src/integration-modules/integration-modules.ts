28aba153573425cf3ac216efbda0a04b1b7fa061
kastov <yk@sent.com>
2026-06-22 04:10:35 +0300

    fix: telegram api module load

--- src/integration-modules/integration-modules.ts
+++ src/integration-modules/integration-modules.ts
@@ -1,12 +1,19 @@
 import { ConditionalModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
+import { isProcessor } from '@common/utils/startup-app';
+
 import { TelegramBotModule } from './notifications/telegram-bot/telegram-bot.module';
 import { WebhookModule } from './notifications/webhook-module/webhook.module';
 
 @Module({
     imports: [
-        ConditionalModule.registerWhen(TelegramBotModule, 'IS_TELEGRAM_NOTIFICATIONS_ENABLED'),
+        ConditionalModule.registerWhen(
+            TelegramBotModule,
+            (env) =>
+                env['IS_TELEGRAM_NOTIFICATIONS_ENABLED']?.toLowerCase() !== 'false' &&
+                isProcessor(),
+        ),
         ConditionalModule.registerWhen(WebhookModule, 'WEBHOOK_ENABLED'),
     ],
 })
